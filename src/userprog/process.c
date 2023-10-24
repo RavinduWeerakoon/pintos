311caa0754be6c2e2cd683ab721b834a547365eb
Ravindu Weerakoon <ravinduhiran26@gmail.com>
2023-10-24 07:44:42 +0530

    process 2

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -111,12 +111,17 @@ start_process (void *file_name_)
     /* If load failed, quit. */
     palloc_free_page (file_name);
   }
+
+  //void hex_dump (uintptr_t ofs, const void *, size_t size, bool ascii);
+  hex_dump(if_.esp , if_.esp , PHYS_BASE -if_.esp , 
+true);
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
      threads/intr-stubs.S).  Because intr_exit takes all of its
      arguments on the stack in the form of a `struct intr_frame',
      we just point the stack pointer (%esp) to our stack frame
      and jump to it. */
+  
   asm volatile ("movl %0, %%esp; jmp intr_exit" : : "g" (&if_) : "memory");
   NOT_REACHED ();
 }
